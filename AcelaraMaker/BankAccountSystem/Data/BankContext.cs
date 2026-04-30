875dbf98823cd44f100a78df74dd1417b2650e4d
Hudson <hudson.henri10@gmail.com>
2026-04-30 10:40:37 -0300

    feat: add IAccountRepository and setup EF Core SQLite BankContext

---
+++ AcelaraMaker/BankAccountSystem/Data/BankContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankAccountSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAccountSystem.Data
+{
+    public class BankContext: DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bank.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasDiscriminator<string>("AccountType")
+                .HasValue<CheckingAccount>("Checking")
+                .HasValue<SavingsAccount>("Savings");
+            modelBuilder.Entity<Account>()
+                .HasKey(a => a.Number);
+        }
+    }
+}
---
+++ AcelaraMaker/BankAccountSystem/Repositories/IAccountRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankAccountSystem
+
+using BankAccountSystem.Models;
+
+namespace BankAccountSystem.Repositories
+{
+    public interface IAccountRepository
+    {
+        void FindByNumber(int number);
+        void ListAll();
+        void Create(Account account);
+        void Update(Account account);
+        void Delete(Account account);
+        void Withdraw(int number, decimal amount);
+        void Deposit(int number, decimal amount);
+        void Transfer(int originNumber, int destinationNumber, decimal amount);
+    }
+}
