c8dab10887950309c7f52d8c41e128c001ce2371
Hudson <hudson.henri10@gmail.com>
2026-05-04 15:57:04 -0300

    feat: add ConsoleColors utility class for terminal styling

---
+++ AcelaraMaker/BankAccountSystem/Utils/ConsoleColors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountSystem.Utils
+{
+    public static class ConsoleColors
+    {
+        public static void ShowHeader(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void ShowSuccess(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        public static void ShowError(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
